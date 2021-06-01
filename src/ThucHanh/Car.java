7ab09aa7ecef421bb74e46b9f838e38ccd43014a
vuongvantu <vuongvantu2001x@gmail.com>
2021-06-01 14:19:09 +0700

    Racing Car

---
+++ src/ThucHanh/Car.java
@@ -0,0 +1,65 @@
+package ThucHanh;
+
+
+import java.util.Random;
+
+import static ThucHanh.Main.DISTANCE;
+import static ThucHanh.Main.STEP;
+
+public class Car implements Runnable{
+    private String name;
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        int runDistance = 0;
+        long startTime = System.currentTimeMillis();
+        while (runDistance < DISTANCE) {
+            try {
+                int speed = (new Random()).nextInt(20);
+                runDistance += speed;
+                String log = "|";
+                int percentTravel = (runDistance * 100) / DISTANCE;
+                for (int i = 0; i < DISTANCE; i += STEP) {
+                    if (percentTravel >= i + STEP) {
+                        log += "=";
+                    } else if (percentTravel >= i && percentTravel < i + STEP) {
+                        log += "o";
+                    } else {
+                        log += "-";
+                    }
+                }
+                log += "|";
+                System.out.println("Car" + this.name + ": " + log + " " + Math.min(DISTANCE, runDistance) + "KM");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Car" + this.name + " broken...");
+                break;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Car" + this.name + " Finish in " + (endTime - startTime) / 1000 + "s");
+    }
+}
+class Main {
+    public static int DISTANCE = 100;
+
+    public static int STEP = 2;
+
+    public static void main(String[] args) {
+        Car carA = new Car("A");
+        Car carB = new Car("B");
+        Car carC = new Car("C");
+
+        Thread thread1 = new Thread(carA);
+        Thread thread2 = new Thread(carB);
+        Thread thread3 = new Thread(carC);
+
+        System.out.println("Distance: 100KM");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
